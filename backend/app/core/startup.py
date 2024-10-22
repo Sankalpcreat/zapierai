365a21062077e5dfeb75517db0d17937154b5407
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-22 14:42:50 +0530

    changes

--- backend/app/core/startup.py
+++
@@ -1,5 +0,0 @@
-from app.db.init_db import init_db
-
-async def on_startup():
-    await init_db()
-    print("Application startup: Database initialized.")
--- backend/app/models/pydantic_schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class TaskSchema(BaseModel):
-    name: str
-
-class ImageTaskSchema(BaseModel):
-    prompt: str
-    style: str
