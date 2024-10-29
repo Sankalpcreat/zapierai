2feaa03e51656195b43d13f79427705482a39aac
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-30 03:03:21 +0530

    tailwind config changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: '#1a202c',
+        secondary: '#2d3748',
+        accent: '#ed64a6',
+        background: '#f9f9f9',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
